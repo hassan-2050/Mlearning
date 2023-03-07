a5bf92efebbad0a9281bd13513a8224cae327079
hassanshah <syedhassan2050@gmail.com>
2023-03-07 20:42:40 +0500

    requirment

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+hyphen='-e .'
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path)as file_obj:
+        requirements=file_obj.readlines()
+        [requirement.replace ("/n","") for requirement in requirements ]
+
+        if hyphen in requirements:
+            requirements.remove(hyphen)
+
+
+
+
+setup(
+
+name="mlproject",
+author="hassan imam",
+
+packages=find_packages(),
+
+install_requires=get_requirements('requirements.txt')
+
+)
+print(packages)
